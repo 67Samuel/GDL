d3b158a7479c48dc93e7fe65abdb908d5906dac6
67Samuel <67ubersam@gmail.com>
2020-11-20 17:26:12 +0800

    created ActivityWithMenu class to enable menus in all activities

---
+++ app/src/main/java/com/example/gdl/HomePage.java
@@ -0,0 +1,27 @@
+package com.example.gdl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePage extends ActivityWithMenu {
+    public static final String ACTIVITY_TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+    }
+
+}
