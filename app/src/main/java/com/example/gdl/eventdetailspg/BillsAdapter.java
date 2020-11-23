37e447ad676d7124d9120ad31feffbb50b5ffb55
beazt123 <lohzhixuan@live.com>
2020-11-23 17:23:19 +0800

    Added event details page. Not reachable from the eventlist pg yet

---
+++ app/src/main/java/com/example/gdl/eventdetailspg/BillsAdapter.java
@@ -0,0 +1,77 @@
+package com.example.gdl.eventdetailspg;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.gdl.R;
+import com.example.gdl.models.Bill;
+import com.example.gdl.models.Event;
+
+import java.util.List;
+
+
+public class BillsAdapter extends ArrayAdapter<Bill> {
+    private int resourceId;
+
+    //save view as cache
+    class ViewHolder {
+        ImageView billsImage;
+        TextView billsName;
+        TextView billsPayer;
+        TextView billsSpending;
+        TextView billsMembers;
+    }
+
+    //instructor, accept data
+    public BillsAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<Bill> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Bill bill = getItem(position); //get event instance
+
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+
+            // Avoid reloading the layout every time the ListView is scrolled
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+
+            // Avoid getting the control instance every time getView() is called
+            viewHolder = new ViewHolder();
+            viewHolder.billsImage = view.findViewById(R.id.bill_image);
+            viewHolder.billsName = view.findViewById(R.id.bill_name);
+            viewHolder.billsPayer = view.findViewById(R.id.bill_payer);
+            viewHolder.billsSpending = view.findViewById(R.id.bill_spending);
+            viewHolder.billsMembers = view.findViewById(R.id.bill_members);
+
+            // save instance of viewHolder to view
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.billsImage.setImageResource(R.drawable.ashketchum);//for test
+        viewHolder.billsName.setText(bill.getName());
+        viewHolder.billsPayer.setText(bill.getPayer().getName());
+        viewHolder.billsMembers.setText("Members: "+bill.memberSize());
+        viewHolder.billsSpending.setText("Total Spending: "+bill.getSpending());
+
+        //TODO: find a way to judge the status of bill
+        /**if (bill.getStatus()){
+            do some thing;
+        } else {
+            do some thing;
+        }*/
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/gdl/eventdetailspg/BillsListFragment.java
@@ -0,0 +1,62 @@
+package com.example.gdl.eventdetailspg;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.gdl.R;
+import com.example.gdl.eventlistpg.EventAdapter;
+import com.example.gdl.models.Bill;
+import com.example.gdl.models.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BillsListFragment extends Fragment {
+
+    //save event data
+    private List<Bill> billsList=new ArrayList<>();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.event_listview, container,false);
+
+        // get the date and use it to create adapter
+        testBills(); //initialize
+        BillsAdapter adapter=new BillsAdapter(getContext(), R.layout.event_item, billsList);
+
+        // transfer the data from adapter to listView
+        ListView listView=view.findViewById(R.id.event_listview);
+        listView.setAdapter(adapter);
+
+        // onClick method for each event
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bill bill=billsList.get(position);
+                Toast.makeText(getContext(), bill.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        return view;
+    }
+
+
+    public void testBills(){
+        for(int i=0;i<5;i++){
+            Bill bill = new Bill();
+            Item item = new Item();
+            bill.addItem(item);
+            billsList.add(bill);
+        }
+    }
+}
