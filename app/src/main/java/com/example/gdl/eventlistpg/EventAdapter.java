c40a61c3a28e52c3bcc7b58a7631595cb825b36a
beazt123 <lohzhixuan@live.com>
2020-11-23 17:14:26 +0800

    Added Events List Page

---
+++ app/src/main/java/com/example/gdl/eventlistpg/EventAdapter.java
@@ -0,0 +1,78 @@
+package com.example.gdl.eventlistpg;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+import com.example.gdl.R;
+import com.example.gdl.models.Event;
+
+
+
+
+public class EventAdapter extends ArrayAdapter<Event> {
+    private int resourceId;
+
+    //save view as cache
+    class ViewHolder {
+        ImageView statusImage;
+        ImageView eventImage;
+        TextView eventName;
+        TextView eventTime;
+        TextView eventSpending;
+        TextView eventMembers;
+    }
+
+    //instructor, accept data
+    public EventAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<Event> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Event event = getItem(position); //get event instance
+
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+
+            // Avoid reloading the layout every time the ListView is scrolled
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+
+            // Avoid getting the control instance every time getView() is called
+            viewHolder = new ViewHolder();
+            viewHolder.eventImage = view.findViewById(R.id.event_image);
+            viewHolder.eventName = view.findViewById(R.id.event_name);
+            viewHolder.eventTime = view.findViewById(R.id.event_time);
+            viewHolder.eventSpending = view.findViewById(R.id.event_spending);
+            viewHolder.eventMembers = view.findViewById(R.id.event_members);
+            viewHolder.statusImage = view.findViewById(R.id.status_image);
+
+            // save instance of viewHolder to view
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.eventImage.setImageResource(event.getImageId());
+        viewHolder.eventName.setText(event.getEventName());
+        viewHolder.eventTime.setText(event.getEventTime());
+        viewHolder.eventMembers.setText("Members: "+event.getEventMembers());
+        viewHolder.eventSpending.setText("Total Spending: "+event.getEventSpending());
+        if (event.getStatus()){
+            viewHolder.statusImage.setImageResource(R.drawable.completed);
+        } else {
+            viewHolder.statusImage.setImageResource(R.drawable.ongoing);
+        }
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/gdl/eventlistpg/EventListActivity.java
@@ -0,0 +1,109 @@
+package com.example.gdl.eventlistpg;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.gdl.ActivityWithMenu;
+import com.example.gdl.createeventpg.CreateEventMain;
+import com.example.gdl.JoinEventActivity;
+import com.example.gdl.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class EventListActivity extends ActivityWithMenu {
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+    FloatingActionButton floatingButton_join;
+    FloatingActionButton floatingButton_create;
+
+    //save fragments
+    List<Fragment> fragments = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_list);
+
+        tabLayout = findViewById(R.id.event_list_tablayout);
+        viewPager = findViewById(R.id.event_list_viewpager);
+        floatingButton_join = findViewById(R.id.eventlist_floatingbutton_join);
+        floatingButton_create = findViewById(R.id.eventlist_floatingbutton_create);
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("Ongoing"));
+        tabLayout.addTab(tabLayout.newTab().setText("Completed"));
+
+
+        //save instances of fragment to fragments
+        fragments.add(new EventListFragment());
+        fragments.add(new EventListFragment());
+
+        viewPager.setAdapter(new FragmentStateAdapter(getSupportFragmentManager(), getLifecycle()) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getItemCount() {
+                return fragments.size();
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+        floatingButton_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EventListActivity.this, JoinEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        floatingButton_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EventListActivity.this, CreateEventMain.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
+
