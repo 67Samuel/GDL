76eec56e4017b19de8a2a5efeae281c2345d6a0e
Mathias Koh <mathias_koh@mymail.sutd.edu.sg>
2020-12-02 11:09:49 +0800

    Standardised aesthetics and added some animation to floating action buttons

--- app/src/main/java/com/example/gdl/eventlistpg/EventListActivity.java
+++ app/src/main/java/com/example/gdl/eventlistpg/EventListActivity.java
@@ -4,6 +4,7 @@ package com.example.gdl.eventlistpg;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -26,6 +27,10 @@ public class EventListActivity extends GDLActivity {
     ViewPager2 viewPager;
     FloatingActionButton floatingButton_join;
     FloatingActionButton floatingButton_create;
+    FloatingActionButton floatingButton_open;
+    Boolean isFABOpen = false;
+    TextView FAB_create_text;
+    TextView FAB_join_text;
 
     //save fragments
     List<Fragment> fragments = new ArrayList<>();
@@ -39,6 +44,9 @@ public class EventListActivity extends GDLActivity {
         viewPager = findViewById(R.id.event_list_viewpager);
         floatingButton_join = findViewById(R.id.eventlist_floatingbutton_join);
         floatingButton_create = findViewById(R.id.eventlist_floatingbutton_create);
+        floatingButton_open = findViewById(R.id.open_FAB_btn);
+        FAB_create_text = findViewById(R.id.create_event_text);
+        FAB_join_text = findViewById(R.id.join_event_text);
 
         tabLayout.addTab(tabLayout.newTab().setText("Ongoing"));
         tabLayout.addTab(tabLayout.newTab().setText("Completed"));
@@ -100,6 +108,50 @@ public class EventListActivity extends GDLActivity {
             }
         });
 
+        floatingButton_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isFABOpen) {
+                    showFABMenu();
+                } else {
+                    closeFABMenu();
+                }
+            }
+        });
+
     }
+
+
+    private void showFABMenu() {
+        isFABOpen = true;
+        floatingButton_create.animate().translationY(-getResources().getDimension(R.dimen.standard_60));
+        floatingButton_join.animate().translationY(-getResources().getDimension(R.dimen.standard_120));
+        floatingButton_open.animate().rotation(45);
+        FAB_join_text.animate().translationY(-getResources().getDimension(R.dimen.standard_120));
+        FAB_create_text.animate().translationY(-getResources().getDimension(R.dimen.standard_60));
+        FAB_join_text.setVisibility(View.VISIBLE);
+        FAB_create_text.setVisibility(View.VISIBLE);
+    }
+
+    private void closeFABMenu() {
+        isFABOpen = false;
+        floatingButton_create.animate().translationY(0);
+        floatingButton_join.animate().translationY(0);
+        floatingButton_open.animate().rotation(0);
+        FAB_join_text.animate().translationY(0);
+        FAB_create_text.animate().translationY(0);
+        FAB_create_text.setVisibility(View.GONE);
+        FAB_join_text.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isFABOpen) {
+            closeFABMenu();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
 
