1d319404966ef48a497c13d7fab700805a7ad6a1
Mathias Koh <mathias_koh@mymail.sutd.edu.sg>
2020-12-02 12:49:40 +0800

    Implemented Add a friend page functionality with referencing the database.

--- app/src/main/java/com/example/gdl/AddFriendPage.java
+++ app/src/main/java/com/example/gdl/AddFriendPage.java
@@ -1,4 +1,4 @@
-package com.example.gdl;
+     package com.example.gdl;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -11,8 +11,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.Query;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.SetOptions;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AddFriendPage extends GDLActivity {
 
@@ -22,8 +35,12 @@ public class AddFriendPage extends GDLActivity {
     Button addButton;
     EditText editTextFriendID;
     TextView personalID;
-    TextView friendName;
+    TextView friendNameTextView;
     ImageView friendPFP;
+    Boolean search_Successful = false;
+    String current_Friend_ID;
+    String current_Friend_Name;
+    ArrayList friend_list;
 
 
     @Override
@@ -35,7 +52,7 @@ public class AddFriendPage extends GDLActivity {
         addButton = findViewById(R.id.addFriendBtn);
         editTextFriendID = findViewById(R.id.editTextFriendID);
         personalID = findViewById(R.id.personalIDNumber);
-        friendName = findViewById(R.id.friendName);
+        friendNameTextView = findViewById(R.id.friendName);
         friendPFP = findViewById(R.id.friendPFP);
 
         FirebaseUser user = mAuth.getCurrentUser();
@@ -54,8 +71,64 @@ public class AddFriendPage extends GDLActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: query database using the editText text, get user info
-                //Query getMember = mRootDatabaseRef.child("Users").equalTo(editTextFriendID.getText().toString());
+                DocumentReference docRef = db.collection("Users").document(editTextFriendID.getText().toString()); //change id to friend's id (editTextFriendID)
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                current_Friend_ID = editTextFriendID.getText().toString();
+                                current_Friend_Name = document.getData().get("name").toString(); //change key to whatever the name is
+                                friendNameTextView.setText(current_Friend_Name);
+                                search_Successful = true;
+                            } else {
+                                //Toast.makeText(AddFriendPage.this, "Friend not found", Toast.LENGTH_LONG).show();
+                                friendNameTextView.setText("Friend not found");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (search_Successful = true) {
+
+                    //get the current friend's list, add this one if it's not inside
+                    DocumentReference docRef = db.collection("Users").document(user.getUid()); //change id to friend's id (editTextFriendID)
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    friend_list = (ArrayList) document.getData().get("friends"); // get friendlist
+                                    if (friend_list.contains(current_Friend_ID) == false) { // if searched user not in list
+                                        friend_list.add(current_Friend_ID); //add to list
+                                        Map <String, Object> friendToAdd = new HashMap<>();
+                                        friendToAdd.put("friends", friend_list);
+                                        db.collection("Users").document(user.getUid()).set(friendToAdd, SetOptions.merge()); //update list
+                                        Toast.makeText(AddFriendPage.this, "Friend added", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(AddFriendPage.this, "Already a friend", Toast.LENGTH_SHORT).show();
+                                }
+                                } else {
+                                    //Toast.makeText(AddFriendPage.this, "Friend not found", Toast.LENGTH_LONG).show();
+                                    friendNameTextView.setText("Friend not found");
+                                }
+                            } else {
+                                Log.d(TAG, "get failed with ", task.getException());
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(AddFriendPage.this, "Search for a friend first", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
