640dc2f1c824a63a89db5720a1d60c7b10202cf1
67Samuel <67ubersam@gmail.com>
2020-12-07 20:35:40 +0800

    implemented database for myfriendspg and shared pref singleton for createeventpg

--- app/src/main/java/com/example/gdl/myfriendspg/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/gdl/myfriendspg/RecyclerViewAdapter.java
@@ -1,21 +1,95 @@
 package com.example.gdl.myfriendspg;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.gdl.Glide.GlideApp;
 import com.example.gdl.R;
+import com.example.gdl.models.Member;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ExampleViewHolder> {
+import de.hdodenhof.circleimageview.CircleImageView;
 
-    private ArrayList<exampleItem> mExampleList;
+public class RecyclerViewAdapter extends RecyclerView.Adapter<com.example.gdl.myfriendspg.RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<Member> mFriendsList;
+    Context context;
+    final StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+    StorageReference userImageStorageRef;
+
+    public RecyclerViewAdapter(Context context, ArrayList<Member> friendsList) {
+        this.mFriendsList = friendsList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public com.example.gdl.myfriendspg.RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.member_list_item, parent, false);
+        return new com.example.gdl.myfriendspg.RecyclerViewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull com.example.gdl.myfriendspg.RecyclerViewAdapter.ViewHolder holder, int position) {
+        //bind specific data to the views in each itemView
+        holder.name.setText((String)mFriendsList.get(position).getName());
+        Log.d(TAG, "onCreate: getting profile pic from storage");
+        StorageReference imageStorageRef = storageRef.child("Profile Images");
+        Log.d(TAG, "onCreate: uid: "+mFriendsList.get(position).getId());
+        userImageStorageRef = imageStorageRef.child(mFriendsList.get(position).getId() + ".jpg");
+        GlideApp.with(holder.profilePhoto.getContext())
+                .load(userImageStorageRef)
+                .into(holder.profilePhoto);
+        //holder.profilePhoto.setImageURI(mFriendsList.get(position).getUriFromString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFriendsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        //after receiving the itemView from onCreateViewHolder, assign the required views to it
+        TextView name;
+        CircleImageView profilePhoto;
+        CardView itemLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.memberName);
+            profilePhoto = itemView.findViewById(R.id.profile_image);
+            itemLayout = itemView.findViewById(R.id.member_recyclerView_item);
+            itemLayout.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Member selectedMember = mFriendsList.get(getAdapterPosition());
+
+        }
+    }
+}
+
+/*public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ExampleViewHolder> {
+
+    private ArrayList<Member> friendsList;
 
 
     public static class ExampleViewHolder extends RecyclerView.ViewHolder {
@@ -34,29 +108,33 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         }
     }
 
-    public RecyclerViewAdapter(ArrayList<exampleItem> exampleList) {
-        mExampleList = exampleList;
+    public RecyclerViewAdapter(ArrayList<Member> friendsList) {
+        this.friendsList = friendsList;
     }
 
     @NonNull
     @Override
     public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listfriend, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.member_list_item, parent, false);
         ExampleViewHolder evh = new ExampleViewHolder(v);
         return evh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ExampleViewHolder holder, int position) {
+
         exampleItem currentItem = mExampleList.get(position);
         holder.mImageView.setImageResource(currentItem.getImageResource());
         holder.mTextView1.setText(currentItem.getText1());
         holder.mTextView2.setText(currentItem.getText2());
         holder.mTextView3.setText(currentItem.getText3());
+
+
     }
 
     @Override
     public int getItemCount() {
-        return mExampleList.size();
+        return friendsList.size();
     }
 }
+*/
