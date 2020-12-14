5f81195cf012e35caf0553292f0f930f62e5d3a5
beazt123 <lohzhixuan@live.com>
2020-12-14 23:00:15 +0800

    Fixed invalid input issue for intent.put()

--- app/src/main/java/com/example/gdl/createeventpg/CreateEventSelectMembers.java
+++ app/src/main/java/com/example/gdl/createeventpg/CreateEventSelectMembers.java
@@ -14,6 +14,7 @@ import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
